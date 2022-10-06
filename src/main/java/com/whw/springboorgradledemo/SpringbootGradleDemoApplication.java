cd000e11eb4ede020b5a1313e0cd0472d01711de
200525112@qq.com <200525112@qq.com>
2022-10-07 00:44:01 +0800

    Init project

---
+++ src/main/java/com/whw/springboorgradledemo/SpringbootGradleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.whw.springboorgradledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGradleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGradleDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whw/springboorgradledemo/SpringbootGradleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whw.springboorgradledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGradleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
